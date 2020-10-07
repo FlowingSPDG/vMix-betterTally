4843bcd8048010009cb05fcb0414627000bb9b44
Shugo Kawamura <shugo_k@flowing.tokyo>
2020-10-07 13:48:39 +0900

    init server side
    TODO : error handling

--- server/main.go
+++ server/main.go
@@ -1,25 +1,99 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
+	"encoding/json"
+	"encoding/xml"
+	"net/http"
 
-	vmix "github.com/FlowingSPDG/vmix-go-TCP"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/olahol/melody.v1"
+
+	"github.com/FlowingSPDG/vmix-go"
+	vmixtcp "github.com/FlowingSPDG/vmix-go-TCP"
 )
 
 var (
-	vm = *&vmix.Vmix{}
+	vm = &vmixtcp.Vmix{}
+	m  = &melody.Melody{}
 )
 
 func init() {
 	logrus.Debugln("START...")
 
+	// Set GIN Production mode
+	gin.SetMode(gin.ReleaseMode)
+
 	var err error // Declare error to avoid "vm" var's shadowing
-	vm, err = vmix.New()
+	vm, err = vmixtcp.New("localhost")
+	if err != nil {
+		panic(err)
+	}
+
+	// Subscribe tally event
+	_, err = vm.SUBSCRIBE("TALLY")
 	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
+	r := gin.Default()
+	m = melody.New()
 
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "index.html")
+	})
+
+	r.GET("/inputs", func(c *gin.Context) {
+		_, body, err := vm.XML()
+		if err != nil {
+			// handle error
+		}
+		v := vmixgo.Vmix{}
+		err = xml.Unmarshal([]byte(body), &v)
+		if err != nil {
+			// handle error
+		}
+		c.JSON(http.StatusOK, v.Inputs.Input)
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		// m.Broadcast(msg)
+	})
+
+	// register callback
+	vm.RegisterTallyCallback(func(res *vmixtcp.TallyResponse) {
+		msg := make(map[string]vmixtcp.TallyStatus)
+		_, body, err := vm.XML()
+		if err != nil {
+			// handle error
+		}
+		v := vmixgo.Vmix{}
+		err = xml.Unmarshal([]byte(body), &v)
+		if err != nil {
+			// handle error
+		}
+
+		logrus.Debugln("TALLY STATUS :", res.Status)
+		for i := 0; i < len(res.Tally); i++ {
+			logrus.Debugf("TALLY [%d] STATUS : %s\n", i+1, res.Tally[i].String())
+			for _, v := range v.Inputs.Input {
+				if int(v.Number) == i+1 { // If input scene matches...
+					msg[v.Key] = res.Tally[i]
+				}
+			}
+		}
+		b, err := json.Marshal(msg)
+		if err != nil {
+			// handle error
+		}
+		m.Broadcast(b)
+	})
+
+	r.Run(":5000")
 }
